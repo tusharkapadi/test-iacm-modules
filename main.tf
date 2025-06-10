c4107d019c12cb8fffc9560492f7ee02afc58674
Tushar Kapadi <tushar.kapadi@gmail.com>
2025-06-09 18:12:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name = var.instance_name
-  }
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "my_ec2" {
+  source         = "./modules/ec2_instance"
+  ami_id         = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+  instance_type  = "t2.micro"
+  instance_name  = "MyEC2Instance"
 }
